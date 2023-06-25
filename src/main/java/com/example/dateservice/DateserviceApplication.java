36c56af2af709901f7f215ef33877a95fda91a85
Your name <wi22b112@technikum-wien.at>
2023-06-25 10:19:25 +0200

    Initial commit

---
+++ src/main/java/com/example/dateservice/DateserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dateservice/DateserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
